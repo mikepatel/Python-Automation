987181facb0d49681a4b44a2168463a314c319ae
Michael Patel <mikepatel@users.noreply.github.com>
2018-10-16 15:02:33 -0400

    Adding initial script

---
+++ accum_matrix.py
@@ -0,0 +1,16 @@
+# Accumulates over elements in matrix
+
+
+'''
+in_matrix = [ 1 1 1
+              2 2 2
+              3 3 3 ]
+
+out_matrix = [ 1 2  3
+               3 6  9
+               6 12 18 ]
+
+
+'''
+
+# IMPORTs
