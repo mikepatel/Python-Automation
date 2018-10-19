d570f1f9303edf7ec11fb65e9492153c9e2f3844
Michael Patel <mikepatel@users.noreply.github.com>
2018-10-18 22:20:03 -0400

    Download ps files success!

--- download_all_file_links.py
+++ download_all_file_links.py
@@ -8,40 +8,38 @@ import os
 
 # website to crawl
 url = "https://web.archive.org/web/20170712033340/http://www.princeton.edu/~verdu/mud/solutions/"
-
 rgx_pattern = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-
 prefix = "https://web.archive.org"
 
 # url to test
-test = "https://web.archive.org/web/20060917084044/http://www.princeton.edu/~verdu/mud/solutions/1/1.3.xinwang.ps"
+test = "https://web.archive.org/web/20060919125641/http://www.princeton.edu/~verdu/mud/solutions/7/7.1.huaidai.ps"
 
 #'''
 
-r = requests.get(url)  #
+r = requests.get(url)  # main site = page with Table of Contents
 data = r.text
 soup = BeautifulSoup(data)
 
-for link in soup.find_all("a"):
-    print(link.get("href"))
-    x = requests.get(link.get("href"), allow_redirects=True)
-    d = x.text
+for link in soup.find_all("a"):  # for each problem
+    #print(link.get("href"))  # link = each problem
+    r = requests.get(link.get("href"), allow_redirects=True)
+    d = r.text
     s = BeautifulSoup(d)
 
-    for l in s.find_all("a"):
+    for sol_link in s.find_all("a"):  # for each solution file to each problem
         try:
-            print(l)
-            y = str(l.get("href"))
-            print("Y: " + y)
-            j = y.split("solutions/")
-            k = j[1].split("/")
-            print("J: " + str(j))
-            print("K: " + str(k))
-            z = prefix + y
-            print("Z: " + z)
-            q = requests.get(z)
+            #print(sol_link)
+            link_string = str(sol_link.get("href"))
+            print("Problem link: " + link_string)
+            url_parts = link_string.split("solutions/")
+            k = url_parts[1].split("/")  # get just problem number and name of soln author
+            #print("J: " + str(j))
+            #print("K: " + str(k))
+            full_problem_url = prefix + link_string
+            #print("Full url for problem: " + full_problem_url)
+            q = requests.get(full_problem_url)
             w = q.url
-            print("W: " + w)
+            print("Url of full problem url: " + w)
             r = requests.get(w, stream=True)
             file = os.path.join(os.getcwd(), k[1])
             print("File: " + file)
@@ -55,13 +53,14 @@ for link in soup.find_all("a"):
 ####
 # test 1 download
 r = requests.get(test, stream=True)
+print(r.status_code)
 print(os.getcwd())
-g = "1/1.3.xinwang.ps"
-g.replace("/", "\\")
+g = "test.ps"
 file = os.path.join(os.getcwd(), g)
 
 
 print(file)
 with open(file, "wb") as f:
     f.write(r.content)
+
 '''
